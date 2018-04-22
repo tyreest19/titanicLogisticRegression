a97e784585b40d0cefaada24a38686bccbfa0321
Ubuntu <ubuntu@ip-172-31-20-17.us-west-2.compute.internal>
2018-04-22 04:00:12 +0000

    Corretcly implemented gradient descent and cost function

--- model.py
+++ model.py
@@ -2,40 +2,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def sigmoid(x, theta):
-    z = np.dot(
-        np.transpose(theta),
-        x
-    )
+def sigmoid(z):
     return 1/(1 + np.exp(-z))
 
 def costFunction(x, y, m, theta):
+    #return np.transpose(y) * np.log(sigmoid(x * theta)) + np.transpose(1 - y) * np.log(1 - sigmoid(x * theta))
     loss = 0
     for i in range(m):
-        loss += y[i] * np.log(sigmoid(x[i], theta)) + (1 - y[i]) * np.log(1 - sigmoid(x[i], theta))
+        z = np.dot(
+            np.transpose(theta),
+            x[i]
+        )
+        loss += y[i] * np.log(sigmoid(z)) + (1 - y[i]) * np.log(1 - sigmoid(z))
     return -(1/m) * loss
 
 def gradientDescent(x, y, m, theta, alpha, iterations=1500):
-    for i in range(iterations):
-        gradient = 0
-        for j in range(m):
-            gradient += (sigmoid(x[j], theta) - y[j]) * x[j]
-        theta = theta - ((alpha/m) * gradient)
-        # print(theta)
+    #return theta - (alpha/m)* np.transpose(x) * (sigmoid(x * theta) - y)
+    for iteration in range(iterations):
+        for j in range(len(theta)):
+            gradient = 0
+            for i in range(m):
+                z = np.dot(
+                    np.transpose(theta),
+                    x[i]
+                )
+                gradient += (sigmoid(z) - y[i]) * x[i]
+            theta[j] = theta[j] - ((alpha/m) * gradient)
         print('Current Error is:', costFunction(x, y, m, theta))
     return theta
+
+def test(x, y, m, theta):
+    correct = 0
+    for i in range(m):
+        pass
 
 if __name__ == '__main__':
 
     training_data = pd.read_csv('data/train.csv')
-    test_data = pd.read_csv('data/test.csv')
     training_data = training_data.dropna() # Removes any rows containing Null values
-    print(len(training_data['Age']) == len(training_data['Survived']))
 
-    theta = np.random.uniform(size=len(training_data['Age']))
-    features = np.asarray([training_data['Age']])
-    actual_values = np.asarray([training_data['Survived']])
-    print('Final theta\'s \n', gradientDescent(features, actual_values, len(features), theta, 0.01))
-    #print(training_data.head())
-    # print(training_data.corr())
-
+    features = np.asarray([[age] for age in training_data['Age']])
+    actual_values = np.asarray(training_data['Survived'])
+    theta = np.random.uniform(size=len(features[0]))
+    #print(theta)
+    #print(len(features))
+    #print(costFunction(features, actual_values, len(features), theta))
+    #print(np.transpose(theta))
+    print('Final theta\'s \n', gradientDescent(features, actual_values, len(features[0]), theta, 0.001))
